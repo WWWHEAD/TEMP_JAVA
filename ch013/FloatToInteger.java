94a0b949864b67e8fab5753c6f864c8066d48fa9
WWWHEAD <rockoff017@gmail.com>
2023-04-21 18:45:34 +0900

    Work13

---
+++ ch013/FloatToInteger.java
@@ -0,0 +1,15 @@
+package ch013;
+
+public class FloatToInteger {
+
+	public static void main(String[] args) {
+		int a;
+		
+		a = (int) 10.0;
+		// a = 10.0; // 오류
+		
+		System.out.println("a = " + a);
+
+	}
+
+}
---
+++ ch013/PrintABC.java
@@ -0,0 +1,10 @@
+package ch013;
+
+public class PrintABC {
+
+	public static void main(String[] args) {
+		System.out.println("\"ABC\\n\"");
+
+	}
+
+}
